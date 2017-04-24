1d0205ab8ef7fc7d6c3aa3b1636495a6a3839a1f
Michael8Zhang <jz11_zhang@hotmail.com>
2017-04-24 16:51:34 +1000

    update

--- EcommerceManerger/src/main/java/com/launch/ecommerce/controller/ECommerceController.java
+++ EcommerceManerger/src/main/java/com/launch/ecommerce/controller/ECommerceController.java
@@ -1,14 +1,17 @@
 package com.launch.ecommerce.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,7 +37,7 @@ public class ECommerceController {
 	}
 	
 	/*
-	 * Add a new customer infomation
+	 * Add a new customer information
 	 */
 	@RequestMapping(value = { "/new" }, method = RequestMethod.GET)
 	public String newCustomer(ModelMap model){
@@ -45,7 +48,7 @@ public class ECommerceController {
 	}
 	
 	/*
-	 * Edit a exist customer infomation
+	 * Edit a exist customer information
 	 */
 	@RequestMapping(value = { "/new" }, method = RequestMethod.POST)
 	public String newCustomer(@Valid ECustomer customer, BindingResult result, ModelMap model){
@@ -53,6 +56,11 @@ public class ECommerceController {
 			return "addcustomer";
 		}
 		
+		if(!service.isEcustomerUnique(customer.getEbayId(), customer.getEbayId())){
+            FieldError ssnError =new FieldError("customer","ebayId",messageSource.getMessage("non.unique.ebayid", new String[]{customer.getEbayId()}, Locale.getDefault()));
+            result.addError(ssnError);
+            return "registration";
+        }
 		service.saveCustomer(customer);
 		model.addAttribute("success", "Customer " + customer.getEbayId() + " added successfully !");	
 		return "success";
